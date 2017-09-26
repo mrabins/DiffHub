adb37023cf4bbb475af704ca1a279737b99f49b7
Mark Rabins <markrabins@gmail.com>
2017-09-25 17:30:57 -0700

    Initial Commit

---
+++ DiffHub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DiffHub
+//
+//  Created by Mark Rabins on 9/25/17.
+//  Copyright © 2017 self. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
